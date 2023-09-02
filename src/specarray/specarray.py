86a9e210a3126fff5c8ded712df5caf1db16882d
Thomas Gölles <thomas.goelles@gmail.com>
2023-09-02 09:00:12 +0200

    deleted sample text

--- src/specarray/specarray.py
+++ src/specarray/specarray.py
@@ -1,10 +1,3 @@
-#   ---------------------------------------------------------------------------------
-#   Copyright (c) Microsoft Corporation. All rights reserved.
-#   Licensed under the MIT License. See LICENSE in project root for information.
-#   ---------------------------------------------------------------------------------
-"""This is a Sample Python file."""
-
-
 from __future__ import annotations
 
 
