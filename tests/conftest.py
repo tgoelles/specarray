b571a1407372f20651821eceab7175db53f4255e
tgoelles <thomas.goelles@gmail.com>
2023-09-21 06:24:59 +0000

    better path

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,7 +12,8 @@ import xarray as xr
 
 
 thisdir = Path(__file__).parent.absolute()
-testdatadir = thisdir / "testdata" / "specim_data" / "capture"
+testdatadir = thisdir / "testdata"
+testdatadir1 = thisdir / "testdata" / "specim_data" / "capture"
 specim_ex_name = "fake"
 
 
@@ -21,25 +22,25 @@ def _save_image_ndarray(prefix=""):
     wavelengths = pd.read_csv(thisdir / "wavelengths.csv")
     wavelengths = list(wavelengths["wavelengths (nm)"].values)
     if prefix == "":
-        data = xr.open_dataset("/workspaces/specarray/tests/testdata/capture.nc")["capture"]
+        data = xr.open_dataset(testdatadir / "capture.nc")["capture"]
     elif prefix == "DARKREF_":
-        data = xr.open_dataset("/workspaces/specarray/tests/testdata/black.nc")["black"]
+        data = xr.open_dataset(testdatadir / "black.nc")["black"]
     elif prefix == "WHITEREF_":
-        data = xr.open_dataset("/workspaces/specarray/tests/testdata/white.nc")["white"]
-    fname = testdatadir / f"{prefix}{specim_ex_name}.hdr"
+        data = xr.open_dataset(testdatadir / "white.nc")["white"]
+    fname = testdatadir1 / f"{prefix}{specim_ex_name}.hdr"
     envi.save_image(fname, data.as_numpy().values, interleave="bil", metadata=dict(wavelength=wavelengths))
 
 
 def gen_data():
-    shutil.rmtree(testdatadir, ignore_errors=True)
-    testdatadir.mkdir(parents=True, exist_ok=True)
+    shutil.rmtree(testdatadir1, ignore_errors=True)
+    testdatadir1.mkdir(parents=True, exist_ok=True)
     _save_image_ndarray()
     _save_image_ndarray("DARKREF_")
     _save_image_ndarray("WHITEREF_")
     # rename all files from .img to .raw
-    for f in testdatadir.glob("*.img"):
+    for f in testdatadir1.glob("*.img"):
         f.rename(f.with_suffix(".raw"))
-    shutil.copy(testdatadir, thisdir / "specim_data_no_darkref")
+    shutil.copy(testdatadir1, thisdir / "specim_data_no_darkref")
 
 
 gen_data
